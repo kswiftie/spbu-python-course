4aa70cd81c73b24e967407abb429a4094527b8b2
kswiftie <nikkol222@mail.ru>
2024-10-14 10:56:33 +0300

    1. Updated module generators
    2. Updated and added new tests for module generators
    
    3. task3.md has been updated

--- project/generators/prime_numbers_gen.py
+++ project/generators/prime_numbers_gen.py
@@ -1,20 +1,59 @@
 import functools
+from typing import Generator, Callable
 
 
-def get_prime_number(gen):
-    # todo: add description and exceptions
-    @functools.wraps(gen)
-    def inner(n):
-        s = gen
-        for i in range(n - 1):
-            next(s)
-        return next(s)
+def get_prime_number(*, stopnumber: int = 1000) -> int:
+    """
+    Decorator for prime_seq_gen that counts the nth prime number
 
-    return inner
+    Parameters
+    ----------
+    stopnumber: int
+        The number limiting the sequence of prime numbers
+
+    Returns
+    -------
+    int
+        prime number by inputted number
+    """
+
+    def wrapper(func: Callable) -> int:
+        @functools.wraps(func)
+        def inner(n: int) -> int:
+            print(n)
+            if n < 1:
+                raise ValueError("Number of prime number must be greater than 0")
+            g = func(stopnumber)
+            try:
+                for i in range(n - 1):
+                    next(g)
+                return next(g)
+            except Exception:
+                raise ValueError(
+                    "Number of prime number must be into generated sequence of prime numbers"
+                )
+
+        return inner
+
+    return wrapper
 
 
-def prime_seq_gen(stopnumber=1000):
-    # todo: add description and exceptions
+def prime_seq_gen(stopnumber: int) -> Generator:
+    """
+    A function that generates sequence of prime numbers before inputted number
+
+    Parameters
+    ----------
+    stopnumber: int
+        The number limiting the sequence of prime numbers
+
+    Returns
+    -------
+    Generator
+        Sequence of prime numbers
+    """
+    if stopnumber < 2:
+        raise ValueError("stopnumber must be greater than 1")
     prime_numbers = [2]
     yield 2
     for n in range(3, stopnumber + 1):
--- tests/test_generators_prime_numbers_generator.py
+++ tests/test_generators_prime_numbers_generator.py
@@ -5,10 +5,10 @@ from project.generators import prime_seq_gen, get_prime_number
 @pytest.mark.parametrize(
     "function, prime_number_number, expected",
     [
-        (get_prime_number(prime_seq_gen()), 12, 37),
-        (get_prime_number(prime_seq_gen()), 13, 41),
-        (get_prime_number(prime_seq_gen()), 6, 13),
-        (get_prime_number(prime_seq_gen(150000)), 12423, 133103),
+        (get_prime_number()(prime_seq_gen), 12, 37),
+        (get_prime_number()(prime_seq_gen), 13, 41),
+        (get_prime_number()(prime_seq_gen), 6, 13),
+        (get_prime_number(stopnumber=150000)(prime_seq_gen), 12423, 133103),
     ],
 )
 def test_(function, prime_number_number, expected):
